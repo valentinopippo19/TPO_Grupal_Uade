4dd335d830604b567849a8e1053ece71497b7e5e
Valentino <vpippo@uade.edu.ar>
2025-04-20 20:03:03 -0300

    Add files via upload

---
+++ comodin.py
@@ -0,0 +1,11 @@
+alumnos = ["Lucía Martínez", "Tomás Ríos", "Valentina López", "Julián Ferreyra", "Martina Sosa"]
+
+notas_temas = [
+    ["8", "Funciones"],
+    ["10", "Bucles"],
+    ["7", "Condicionales"],
+    ["9", "Listas"],
+    ["6", "Diccionarios"]
+]
+
+profesores = ["Prof. Ramírez", "Prof. Herrera", "Prof. Suárez", "Prof. Medina", "Prof. Blanco"]
